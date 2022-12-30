e80c3e2951e3b776eded7502754b072a4838965e
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2022-12-30 20:39:51 +0900

    changed tube_list to tube_range_list

--- src/hrrr/dataset/_dataset.py
+++ src/hrrr/dataset/_dataset.py
@@ -1,9 +1,25 @@
+import numpy as np
+
+
+def _tude_list(min_tude, max_tude, length) -> list:
+    delta_tude = (max_tude-min_tude) / (length-1)
+
+    return [min_tude+delta_tude*_i for _i in range(length)]
+
+
 class hrrr_dataset:
     def __init__(self, ds):
         self.ds = ds
 
-    def tude_list(self, min_tude, max_tude, len_list) -> list:
-        delta_tude = (max_tude-min_tude) / (len_list-1)
+    def tude_range_list(self, tude: str) -> list:
+        if tude == 'latitude':
+            min_tude, max_tude = np.min(self.ds.latitude), np.max(self.ds.latitude)
+            length = np.shape(self.ds.t[0])
+        elif tude == 'longitude':
+            min_tude, max_tude = np.min(self.ds.longitude), np.max(self.ds.longitude)
+            length = np.shape(self.ds.t[0])
+        else:
+            raise ValueError(f'You must select latitude or longitude as tude. {tude} is not them.')
 
-        return [min_tude+delta_tude*_i for _i in range(len_list)]
+        return _tude_list(min_tude, max_tude, length)
 
