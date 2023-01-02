d1f3ec40ca649dfc8f6333d81a1344e562319c8f
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-01-02 16:50:31 +0900

    moved some func to _tools.py

--- src/satellite/image/_image.py
+++ src/satellite/image/_image.py
@@ -1,46 +1,12 @@
 import numpy as np
 import pandas as pd
-from datetime import timedelta
-import geopy.distance as distance
 import planetary_computer as pc
 import odc.stac
 import rioxarray
 import cv2
 import collections
 
-
-# get our bounding box to search latitude and longitude coordinates
-def get_bounding_box(latitude, longitude, meter_buffer=3000):
-    """
-    Given a latitude, longitude, and buffer in meters, returns a bounding
-    box around the point with the buffer on the left, right, top, and bottom.
-
-    Returns a list of [minx, miny, maxx, maxy]
-    """
-    distance_search = distance.distance(meters=meter_buffer)
-
-    # calculate the lat/long bounds based on ground distance
-    # bearings are cardinal directions to move (south, west, north, and east)
-    min_lat = distance_search.destination((latitude, longitude), bearing=180)[0]
-    min_long = distance_search.destination((latitude, longitude), bearing=270)[1]
-    max_lat = distance_search.destination((latitude, longitude), bearing=0)[0]
-    max_long = distance_search.destination((latitude, longitude), bearing=90)[1]
-
-    return [min_long, min_lat, max_long, max_lat]
-
-
-# get our date range to search, and format correctly for query
-def get_date_range(date, time_buffer_days=15):
-    """Get a date range to search for in the planetary computer based
-    on a sample's date. The time range will include the sample date
-    and time_buffer_days days prior
-
-    Returns a string"""
-    datetime_format = "%Y-%m-%dT"
-    range_start = pd.to_datetime(date) - timedelta(days=time_buffer_days)
-    date_range = f"{range_start.strftime(datetime_format)}/{pd.to_datetime(date).strftime(datetime_format)}"
-
-    return date_range
+from .. import tools
 
 
 def crop_sentinel_image(item, bounding_box, col_name='visual'):
@@ -132,7 +98,7 @@ def select_best_item(items, date, latitude, longitude):
     )
     if item_details["sentinel"].any():
         item_details = item_details[item_details["sentinel"] == True]
-        feature_bbox = get_bounding_box(latitude, longitude, meter_buffer=200)
+        feature_bbox = tools.get_bounding_box(latitude, longitude, meter_buffer=200)
 
         # Is there water cell in sentinel?
         # ref: https://docs.sentinel-hub.com/api/latest/data/sentinel-2-l2a/#units
