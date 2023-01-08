482b0ccca26db693e206985c386e0bbf4fa60444
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-01-08 16:00:13 +0900

    add parameter module

--- src/satellite/image/__init__.py
+++ src/satellite/image/__init__.py
@@ -1,1 +1,3 @@
 from ._image import *
+from . import convert, crop, resize, parameter
+
---
+++ src/satellite/image/parameter/_detail.py
@@ -0,0 +1,53 @@
+import pystac
+
+from .. import crop
+
+
+def water_dict(item: pystac.item.Item,  bounding_box: list) -> dict:
+    """
+    Create dict which has information whether  water region is included in an image
+
+    Parameters
+    ----------
+    item : Item
+        Pystac Item
+    bounding_box : list
+        Return of tools.get_bounding_box()
+
+    Returns
+    -------
+    water_dict : dict
+        Water key has dict
+
+    """
+
+    return {"datetime": item.datetime.strftime("%Y-%m-%d"),
+            "platform": item.properties["platform"],
+            "water": water_bool(item, bounding_box)}
+
+
+def water_bool(item: pystac.item.Item,  bounding_box: list) -> bool:
+    """
+    Bool whether item image has water region
+
+    Parameters
+    ----------
+    item : Item
+        Pystac Item
+    bounding_box : list
+        Return of tools.get_bounding_box()
+
+    Returns
+    -------
+    water_bool : bool
+        Return True If the image has water region
+
+    """
+
+    _platform = item.properties['platform']
+    print(_platform)
+    if 'sentinel' in _platform.lower():
+        class_array = crop.crop_sentinel_image(item, bounding_box, col_name='SCL')
+    else:
+        class_array = crop.crop_sentinel_image(item, bounding_box, col_name='cloud_qa')
+
