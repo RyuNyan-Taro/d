69a45907601eaf19b8c007e9acc5b09bf69ac16e
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-01-08 15:59:54 +0900

    add square_array

--- src/satellite/image/resize/_resize.py
+++ src/satellite/image/resize/_resize.py
@@ -23,3 +23,26 @@ def twice_len_array(base_array: np.ndarray, resize_len: int) -> np.ndarray:
         resized_array.append([base_array[_i//2][_j//2] for _j in resize_range])
 
     return np.array(resized_array)
+
+
+def square_array(base_array: np.ndarray) -> np.ndarray:
+    """
+    Resize n x n array from n x m array
+    Parameters
+    ----------
+    base_array : ndarray
+        n x m shape array
+
+    Returns
+    -------
+    square_array : ndarray
+        2D array which has n x n shape
+
+    """
+
+    n_shape = base_array.shape[0]
+    square_list = list()
+    for _one_dim_array in base_array:
+        square_list.append(list(_one_dim_array)[:n_shape])
+
+    return np.array(square_list)
