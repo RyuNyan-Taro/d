5b253a8a5713c2d906e18f227f1e12645b075593
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-01-08 11:38:18 +0900

    add _resize.py

---
+++ src/satellite/image/resize/_resize.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def resize_array(base_array: np.array, resize_len: int) -> np.ndarray:
+    """
+    Return resized 2D array of resize_len x resize_len
+    Parameters
+    ----------
+    base_array : np.array
+        2D array
+    resize_len : int
+        Length of return array
+
+    Returns
+    -------
+        resized_array : np.ndarray
+            2D array which resized to resize_len
+
+    """
+    resized_array = list()
+    resize_range = range(resize_len)
+    for _i in resize_range:
+        resized_array.append([base_array[_i//2][_j//2] for _j in resize_range])
+
+    return np.array(resized_array)
