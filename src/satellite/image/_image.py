14d485f114161dbe215c328fb9b87d1362bb3fd6
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-01-08 17:38:42 +0900

    modified logic to select best image

--- src/satellite/image/_image.py
+++ src/satellite/image/_image.py
@@ -1,13 +1,11 @@
 import numpy as np
 import pandas as pd
-import collections
 
-from .. import tools
-from . import crop
+from . import parameter
 
 
 # Refactor our process from above into functions
-def select_best_item(items, date, latitude, longitude):
+def select_best_item(items, date, latitude, longitude, feature_bbox):
     """
     Select the best satellite item given a sample's date, latitude, and longitude.
     If any Sentinel-2 imagery is available, returns the closest sentinel-2 image by
@@ -26,6 +24,7 @@ def select_best_item(items, date, latitude, longitude):
                 "min_lat": item.bbox[1],
                 "max_lat": item.bbox[3],
                 "item_obj": item,
+                "water_region": parameter.water_bool(item, feature_bbox)
             }
             for item in items
         ]
@@ -47,25 +46,14 @@ def select_best_item(items, date, latitude, longitude):
         item_details["datetime"]
     )
 
-    # if we have sentinel-2, filter to sentinel-2 images only
+    # add sentinel-2
     item_details["sentinel"] = item_details.platform.str.lower().str.contains(
         "sentinel"
     )
-    if item_details["sentinel"].any():
+    if item_details["water"].any():
+        item_details = item_details[item_details["water"] == True]
+    elif item_details["sentinel"].any():
         item_details = item_details[item_details["sentinel"] == True]
-        feature_bbox = tools.get_bounding_box(latitude, longitude, meter_buffer=200)
-
-        # Is there water cell in sentinel?
-        # ref: https://docs.sentinel-hub.com/api/latest/data/sentinel-2-l2a/#units
-        try:
-            item_details['scl_water'] = [6 in collections.Counter(np.ravel(_scl_array)).keys()
-                                         for _scl_array
-                                         in [crop.crop_sentinel_image(_item, feature_bbox, col_name='SCL')
-                                         for _item in item_details.item_obj]]
-            if np.sum(item_details['scl_water']) >= 1:
-                item_details = item_details[item_details["scl_water"] == True]
-        except Exception:
-            print('scl_water error')
 
     # return the closest imagery by time
     best_item = item_details.sort_values(by="time_diff", ascending=True).iloc[0]
