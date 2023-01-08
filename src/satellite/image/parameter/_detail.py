859162f446b671859bf281a866517e4fb4d08248
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-01-08 17:06:04 +0900

    add dict value

--- src/satellite/image/parameter/_detail.py
+++ src/satellite/image/parameter/_detail.py
@@ -23,7 +23,8 @@ def water_dict(item: pystac.item.Item,  bounding_box: list) -> dict:
 
     return {"datetime": item.datetime.strftime("%Y-%m-%d"),
             "platform": item.properties["platform"],
-            "water": water_bool(item, bounding_box)}
+            "water": water_bool(item, bounding_box),
+            "item_obj": item}
 
 
 def water_bool(item: pystac.item.Item,  bounding_box: list) -> bool:
@@ -47,11 +48,19 @@ def water_bool(item: pystac.item.Item,  bounding_box: list) -> bool:
     _platform = item.properties['platform']
     print(_platform)
     if 'sentinel' in _platform.lower():
-        class_array = crop.crop_sentinel_image(item, bounding_box, col_name='SCL')[0]
+        try:
+            class_array = crop.crop_sentinel_image(item, bounding_box, col_name='SCL')[0]
+        except:
+            return False
         water_num = 6
-    else:
-        class_array = crop.crop_sentinel_image(item, bounding_box, col_name='cloud_qa')[0]
+    elif _platform == 'landsat_7':
+        try:
+            class_array = crop.crop_sentinel_image(item, bounding_box, col_name='cloud_qa')[0]
+        except:
+            return False
         water_num = 32
+    else:
+        return False
 
     return water_num in class_array
 
